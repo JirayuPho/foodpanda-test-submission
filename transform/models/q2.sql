cf6b40fcb85d1c70ff1a0f875b8b0447a12ec2a1
JirayuPho <134268233+JirayuPho@users.noreply.github.com>
2023-05-22 18:03:25 +0700

    q2.sql

--- transform/models/q2.sql
+++ transform/models/q2.sql
@@ -1,1 +1,18 @@
+{{
+    config(
+        materialized='view'
+    )
+}}
 
+WITH q2 AS (
+    SELECT
+      vendor_name,
+      count(ditinct id) customer_count,
+      count(ditinct id) total_gmv
+      from vendors
+      group by 1
+      where country_name = "Taiwan"
+  
+)
+
+select country_name, customer_count, total_gmv from q2
