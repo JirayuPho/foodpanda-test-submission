a79198f55e5902660358d2da79701e8291986a73
JirayuPho <134268233+JirayuPho@users.noreply.github.com>
2023-05-22 18:07:48 +0700

    Update q3.sql

--- transform/models/q3.sql
+++ transform/models/q3.sql
@@ -1,1 +1,17 @@
+{{
+    config(
+        materialized='view'
+    )
+}}
 
+WITH q3 AS (
+    SELECT
+      country_name,
+      vendor_name,
+      count(ditinct id) total_gmv
+      from vendors
+      group by 1,2
+  
+)
+
+select country_name, vendor_name, total_gmv from q3
