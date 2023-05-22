3a2cfb1538a196bb47a7bf8901c6d7b48c480717
JirayuPho <134268233+JirayuPho@users.noreply.github.com>
2023-05-22 17:56:42 +0700

    q1.sql

--- transform/models/q1.sql
+++ transform/models/q1.sql
@@ -1,1 +1,16 @@
+{{
+    config(
+        materialized='view'
+    )
+}}
 
+WITH q1 AS (
+    SELECT
+      country_name,
+      sum(gmv_local) total_gmv
+      from orders
+      group by 1
+  
+)
+
+select country_name, total_gmv from q1
