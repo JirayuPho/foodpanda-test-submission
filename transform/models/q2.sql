7d7011c7c72485b52b22d98ecb5bfab2091c502c
JirayuPho <134268233+JirayuPho@users.noreply.github.com>
2023-05-22 18:31:54 +0700

    Update q2.sql

--- transform/models/q2.sql
+++ transform/models/q2.sql
@@ -6,12 +6,13 @@
 
 WITH q2 AS (
     SELECT
+      orders.country_name country_name,
       vendors.vendor_name vendor_name,
       count(distinct orders.customer_id) customer_count,
       sum(orders.gmv_local) total_gmv
       from orders left join vendors on orders.vendor_id = vendors.id
-      group by 1
+      group by 1,2
   
 )
 
-select vendor_name,customer_count,total_gmv from q2
+select vendor_name,customer_count,total_gmv from q2 where country_name = "Taiwan"
