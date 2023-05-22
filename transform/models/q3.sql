09db6be092f83b6e4c29ca3bee269177da085c21
JirayuPho <134268233+JirayuPho@users.noreply.github.com>
2023-05-22 18:30:23 +0700

    Update q3.sql

--- transform/models/q3.sql
+++ transform/models/q3.sql
@@ -6,12 +6,12 @@
 
 WITH q3 AS (
     SELECT
-      country_name,
-      vendor_name,
-      count(ditinct id) total_gmv
-      from vendors
+      orders.country_name country_name,
+      vendors.vendor_name vendor_name,
+      sum(orders.gmv_local) total_gmv
+      from orders left join vendors on orders.vendor_id = vendors.id
       group by 1,2
   
 )
 
-select country_name, vendor_name, total_gmv from q3
+select country_name,vendor_name,total_gmv from q3
